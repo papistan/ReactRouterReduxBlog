330a40f39bf2854a08793231d60f9311fb41c9c7
papistan <jpapisan@gmail.com>
2017-11-05 13:37:42 -0800

    Add react-router and proptypes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+	render() { return <div>Hello!</div>}
+};
+
+class Goodbye extends React.Component {
+	render() { return <div>Goodbye!</div>}
+};
+
+
+ReactDOM.render(
+ <Provider store={createStoreWithMiddleware(reducers)}>
+		<Router>
+			<div>
+			<Route path="/hello" component={ Hello } />
+			<Route path="/goodbye" component={ Goodbye } />
+			</div>
+		</Router>
+ </Provider>
+, document.getElementById('root'));
+
 registerServiceWorker();
+

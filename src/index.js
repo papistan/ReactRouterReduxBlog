eab3b94c561ffd24b2e0e5397997e569610e719e
papistan <jpapisan@gmail.com>
2018-01-03 10:45:41 -0800

    Adds reducer and action for posts fetch

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import PostsIndex from './components/PostsIndex';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-	render() { return <div>Hello!</div>}
-};
-
-class Goodbye extends React.Component {
-	render() { return <div>Goodbye!</div>}
-};
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
  <Provider store={createStoreWithMiddleware(reducers)}>
 		<Router>
 			<div>
-				<Route path="/" component={ PostIndex } />
-				<Route path="/posts/:id" component={ PostsShow } />
-				<Route path="/posts/new" component={ PostsNew } />
+				<Route path="/" component={ PostsIndex } />
+				<Route path="/posts/:id" component={ PostsIndex } />
+				<Route path="/posts/new" component={ PostsIndex } />
 			</div>
 		</Router>
  </Provider>

e6791725c06680d008c5549a36b62c3aeb7624c3
papistan <jpapisan@gmail.com>
2018-01-03 11:59:24 -0800

    Adds render posts method in postindex component

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,11 @@
+import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
 
 
 export default function(state = {}, action) {
 	switch (action.type) {
 		case FETCH_POSTS: 
-			console.log(action.payload.data); // [ post1, post2 ]
-			// { 4: post }
+			return _.mapKeys(action.payload.data, 'id')
 		default:
 			return state;
 	}
